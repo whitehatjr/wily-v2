0b47877d9d6d5a96d9b8060bfcf639f6e1bb0be9
vishalgaddam873 <vishal18@navgurukul.org>
2021-03-12 23:59:25 +0530

    Login Screen created

---
+++ screens/Login.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Text
+} from "react-native";
+
+import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
+import * as Font from "expo-font";
+
+const bgImage = require("../assets/background1.png");
+const appIcon = require("../assets/appIcon.png");
+const appName = require("../assets/appName.png");
+
+export default class LoginScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fontLoaded: false
+    };
+  }
+
+  async loadFonts() {
+    await Font.loadAsync({
+      Rajdhani_600SemiBold: Rajdhani_600SemiBold
+    });
+    this.setState({ fontLoaded: true });
+  }
+
+  componentDidMount() {
+    this.loadFonts();
+  }
+
+  render() {
+    const { fontLoaded } = this.state;
+    if (fontLoaded) {
+      return (
+        <View style={styles.container}>
+          <ImageBackground source={bgImage} style={styles.bgImage}>
+            <View style={styles.upperContainer}>
+              <Image source={appIcon} style={styles.appIcon} />
+              <Image source={appName} style={styles.appName} />
+            </View>
+            <View style={styles.lowerContainer}>
+              <TextInput
+                style={styles.textinput}
+                onChangeText={text => {}}
+                placeholder={"Name"}
+              />
+              <TextInput
+                style={[styles.textinput, { marginTop: 20 }]}
+                onChangeText={text => {}}
+                placeholder={"Enter Password"}
+              />
+              <TouchableOpacity style={[styles.button, { marginTop: 20 }]}>
+                <Text style={styles.buttonText}>Login</Text>
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
+        </View>
+      );
+    }
+
+    return null;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF"
+  },
+  bgImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+
+  upperContainer: {
+    flex: 0.6,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  appIcon: {
+    width: 280,
+    height: 280,
+    resizeMode: "contain",
+    marginTop: 80
+  },
+  appName: {
+    width: 130,
+    height: 130,
+    resizeMode: "contain"
+  },
+  lowerContainer: {
+    flex: 0.4,
+    justifyContent: "center",
+    alignItems: "center",
+    borderTopRightRadius: 76,
+    borderTopLeftRadius: 76,
+    backgroundColor: "#FFFFFF",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 13,
+    elevation: 10
+  },
+  textinput: {
+    width: "75%",
+    height: 55,
+    padding: 10,
+    borderColor: "gray",
+    borderWidth: 2,
+    borderRadius: 10,
+    fontSize: 18
+  },
+  button: {
+    width: "43%",
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F48D20",
+    borderRadius: 15
+  },
+  buttonText: {
+    fontSize: 24,
+    color: "#FFFFFF",
+    fontFamily: "Rajdhani_600SemiBold"
+  }
+});
